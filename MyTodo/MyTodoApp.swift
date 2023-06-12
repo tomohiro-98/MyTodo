b61d4e163052bb152292733fec6cbe928ed1dbe6
tomohiro-98 <tomohiro_kuba@mail.toyota.co.jp>
2023-06-12 18:33:47 +0900

    Initial Commit

---
+++ MyTodo/MyTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoApp.swift
+//  MyTodo
+//
+//  Created by user on 2023/06/12.
+//
+
+import SwiftUI
+
+@main
+struct MyTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
